0f79e1ddd84cb3811f4d199078caefc011a3c86a
Naman Sood <mail@nsood.in>
2024-02-25 21:09:28 -0500

    don't recompute on every request
    Signed-off-by: Naman Sood <mail@nsood.in>

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"compress/gzip"
+	"io"
 	"log"
 	"net/http"
 	"strings"
 )
 
 func main() {
+	var buf bytes.Buffer
+	encoder := gzip.NewWriter(&buf)
+	written := 0
+	content := "<b>hi</b><br>"
+	for written < 1024*1024*1024 {
+		encoder.Write([]byte(content))
+		written += len(content)
+	}
+	encoder.Close()
+	log.Printf("ready")
+
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if enc := r.Header.Get("Accept-Encoding"); !strings.Contains(enc, "gzip") {
 			http.Error(w, "no", http.StatusNotAcceptable)
@@ -18,15 +31,7 @@ func main() {
 		w.Header().Add("Content-Type", "text/html")
 		w.WriteHeader(http.StatusOK)
 
-		encoder := gzip.NewWriter(w)
-		defer encoder.Close()
-
-		written := 0
-		content := "<b>hi</b><br>"
-		for written < 1024*1024*1024 {
-			encoder.Write([]byte(content))
-			written += len(content)
-		}
+		io.Copy(w, bytes.NewReader(buf.Bytes()))
 	})
 	log.Fatal(http.ListenAndServe(":1024", handler))
 }
